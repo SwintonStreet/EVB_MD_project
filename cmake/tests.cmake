66fd46541c99c23f7f5cf1291f0d95d83db4ab2c
SwintonStreet <jake.stinson.236@gmail.com>
2022-07-09 20:42:10 +0100

    Improve test case generation to have targetable names
    Also, make a tests target that compiles and runs all the tests in
    parallel based on the number of available processors.

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,31 +1,62 @@
 include(CMakeParseArguments)
+include(ProcessorCount)
+
 find_package(GTest 1.12.1 REQUIRED)
+ProcessorCount(N)
 
 enable_testing()
 
-function(add_unit_test)
+add_custom_target(tests)
+
+add_custom_command(
+    TARGET tests POST_BUILD
+    COMMENT "Run tests"
+    COMMAND ${CMAKE_CTEST_COMMAND} -C "$<CONFIGURATION>"
+                                   --output-on-failures
+                                   -j "${N}"
+    VERBATIM
+)
+
+add_custom_command(
+    TARGET tests PRE_BUILD
+    COMMENT "Ensure tests are configured"
+    COMMAND "${CMAKE_COMMAND}" -B "${CMAKE_BINARY_DIR}"
+                               -G${CMAKE_GENERATOR}
+                               "${CMAKE_HOME_DIRECTORY}"
+    VERBATIM
+)
+
+function(add_unit_tests)
     cmake_parse_arguments(
         PARSED_ARGS # prefix of output variables
-        "" # list of names of the boolean arguments (only defined ones will be true)
-        "" # list of names of mono-valued arguments
-        "SRCS;DEPS" # list of names of multi-valued arguments (output variables are lists)
-        ${ARGN} # arguments of the function to parse, here we take the all original ones
+        "" # list of names of the boolean arguments
+           # (only defined ones will be true)
+        "PREFIX" # list of names of mono-valued arguments
+        "SRCS;DEPS" # list of names of multi-valued arguments
+                    # (output variables are lists)
+        ${ARGN} # arguments of the function to parse,
+                # here we take the all original ones
     )
+
+    add_custom_target(${PARSED_ARGS_PREFIX})
+    add_dependencies(tests ${PARSED_ARGS_PREFIX})
 
     foreach(src ${PARSED_ARGS_SRCS})
         # get the base name
         string(REPLACE "." ";" src_list ${src})
         list(GET src_list 0 src_base)
+        set(target "${PARSED_ARGS_PREFIX}-${src_base}")
 
-        add_executable(${src_base} ${src})
+        add_executable(${target} ${src})
+        add_dependencies(${PARSED_ARGS_PREFIX} ${target})
 
-        include_directories(${src_base} ${GTEST_INCLUDE_DIR})
-        target_link_libraries(${src_base}
+        include_directories(${target} ${GTEST_INCLUDE_DIR})
+        target_link_libraries(${target}
             PRIVATE
             ${PARSED_ARGS_DEPS}
             ${GTEST_LIBRARIES})
 
-        add_test(NAME "${src_base}" COMMAND ${src_base})
+        add_test(NAME "${target}" COMMAND ${target})
     endforeach(src)
 
 endfunction()
